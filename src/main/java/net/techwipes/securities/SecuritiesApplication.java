7b71c6d63b9951b99a916421b1058e6e3e92a743
Mitya <techwipes@gmail.com>
2020-09-12 12:12:36 +0500

    first init, entry point

---
+++ src/main/java/net/techwipes/securities/SecuritiesApplication.java
@@ -0,0 +1,13 @@
+package net.techwipes.securities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritiesApplication.class, args);
+	}
+
+}
