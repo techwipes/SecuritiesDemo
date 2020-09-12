2206e1d7187092ab09f44c9c73bc5ec37beefde1
Mitya <techwipes@gmail.com>
2020-09-12 14:35:41 +0500

    add  constructor (@Autowired) , method createSecurityForm and createSecurity

--- src/main/java/net/techwipes/securities/controller/SecurityController.java
+++ src/main/java/net/techwipes/securities/controller/SecurityController.java
@@ -3,19 +3,43 @@ package net.techwipes.securities.controller;
 
 import net.techwipes.securities.model.Security;
 import net.techwipes.securities.service.SecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.List;
 
 @Controller
 public class SecurityController {
 
-    private SecurityService securityService;
+
+    private final SecurityService securityService;
+
+    @Autowired
+    public SecurityController(SecurityService securityService) {
+        this.securityService = securityService;
+    }
 
     @GetMapping("/securities")
-    public String findAll(){
-        List<Security> securityList = securityService.findAll();
+    public String findAll(Model model){
+        List<Security> securities = securityService.findAll();
+        model.addAttribute("securities", securities);
 
-        return "";
+        return "security-list";
     }
+
+    @GetMapping("/security-create")
+    public String createSecurityForm(Security security){
+        return "security-create";
+    }
+
+    @PostMapping
+    public String createSecurity(Security security){
+        securityService.createSecurity(security);
+        return  "redirect:/securities";
+    }
+
+
 }
