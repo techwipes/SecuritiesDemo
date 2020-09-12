ade4a13a4ea54a4c2bac677179bfd0eb99e38fdd
Mitya <techwipes@gmail.com>
2020-09-12 15:15:44 +0500

    add all CRUD methods

--- src/main/java/net/techwipes/securities/controller/SecurityController.java
+++ src/main/java/net/techwipes/securities/controller/SecurityController.java
@@ -7,13 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class SecurityController {
-
 
     private final SecurityService securityService;
 
@@ -39,6 +39,24 @@ public class SecurityController {
     public String createSecurity(Security security){
         securityService.createSecurity(security);
         return  "redirect:/securities";
+    }
+    @GetMapping("security-delete/{id}")
+    public String deleteSecurity(@PathVariable("id") Integer id){
+        securityService.deleteById(id);
+        return "redirect:/securities";
+    }
+
+    @GetMapping("/security-update/{id}")
+    public String updateSecurityForm(@PathVariable("id")  Integer id, Model model){
+        Security security = securityService.findById(id);
+        model.addAttribute("security", security);
+        return "security-update";
+    }
+
+    @PostMapping("/security-update")
+    public String updateUser(Security security){
+        securityService.createSecurity(security);
+        return "redirect:/security";
     }
 
 
