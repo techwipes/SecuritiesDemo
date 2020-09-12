3829ff7475279c028b5ac967a093506d2c1dee90
Mitya <techwipes@gmail.com>
2020-09-12 15:01:31 +0500

    syntax fix

--- src/main/java/net/techwipes/securities/SecuritiesApplication.java
+++ src/main/java/net/techwipes/securities/SecuritiesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecuritiesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecuritiesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritiesApplication.class, args);
+    }
 
 }
